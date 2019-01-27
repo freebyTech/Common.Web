063a15e1215e9ff9f7df90b745c5e4c01ef755ef
James Eby <temporafugiunt@gmail.com>
2019-01-26 21:49:07 -0600

    Added AppBuilder support for adding Api Exception Middleware.

---
+++ src/freebyTech.Common.Web/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace freebyTech.Common.Web.Middleware
+{
+    public static class MiddlewareExtensions
+    {
+        public static void ConfigureApiExceptionMiddleware(this IApplicationBuilder app)
+        {
+            app.UseMiddleware<ApiExceptionMiddleware>();
+        }
+    }
+}
